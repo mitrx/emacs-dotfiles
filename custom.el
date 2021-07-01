23a8af12edebcd33e842d07fbe7cc610fd0f3b9f
Dmitry Alexeev <me@mitrx.dev>
2021-07-01 15:59:17 +0300

    Tmp

---
+++ custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2b502f6e3bf0cba42fe7bf83a000f2d358a7020a7780a8682fcfed0c9dbffb5f" "076ee9f2c64746aac7994b697eb7dbde23ac22988d41ef31b714fc6478fee224" "c7f364aeea0458b6368815558cbf1f54bbdcc1dde8a14b5260eb82b76c0ffc7b" "7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6" "96c56bd2aab87fd92f2795df76c3582d762a88da5c0e54d30c71562b7bf9c605" default))
+ '(package-selected-packages
+   '(ox-hugo git-link cmake-mode company-glsl glsl-mode zenburn-theme which-key sql-indent rainbow-delimiters projectile modus-vivendi-theme modus-operandi-theme magit leuven-theme kubel git-timemachine counsel company clj-refactor auto-compile ace-window))
+ '(safe-local-variable-values
+   '((cider-clojure-cli-global-options . "-A:fig:dev")
+     (cider-clojure-cli-global-options . "-A:figwheel-main-dev")
+     (cider-clojure-cli-global-options . "-M:figwheel-main-dev")
+     (cider-clojure-cli-global-options . "-A:dev")
+     (cider-ns-refresh-after-fn . "integrant.repl/resume")
+     (cider-ns-refresh-before-fn . "integrant.repl/suspend")))
+ '(send-mail-function 'smtpmail-send-it)
+ '(smtpmail-default-smtp-server "smtp.yandex.ru")
+ '(smtpmail-smtp-server "smtp.yandex.ru")
+ '(smtpmail-smtp-service 465)
+ '(smtpmail-stream-type 'ssl)
+ '(user-mail-address "me@mitrx.dev")
+ '(visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
