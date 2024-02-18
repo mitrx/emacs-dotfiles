8b0a0389a6a6baa1d057b157f0fcfa664cfe4d95
Dmitry Alexeev <me@mitrx.dev>
2024-02-18 21:17:18 +0100

    Add some packages

--- custom.el
+++ custom.el
@@ -4,11 +4,29 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("2b502f6e3bf0cba42fe7bf83a000f2d358a7020a7780a8682fcfed0c9dbffb5f" "076ee9f2c64746aac7994b697eb7dbde23ac22988d41ef31b714fc6478fee224" "c7f364aeea0458b6368815558cbf1f54bbdcc1dde8a14b5260eb82b76c0ffc7b" "7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6" "96c56bd2aab87fd92f2795df76c3582d762a88da5c0e54d30c71562b7bf9c605" default))
+   '("69f7e8101867cfac410e88140f8c51b4433b93680901bb0b52014144366a08c8" "7887cf8b470098657395502e16809523b629249060d61607c2225d2ef2ad59f5" "cca1d386d4a3f645c2f8c49266e3eb9ee14cf69939141e3deb9dfd50ccaada79" "bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" "dde643b0efb339c0de5645a2bc2e8b4176976d5298065b8e6ca45bc4ddf188b7" "896e4305e7c10f3217c5c0a0ef9d99240c3342414ec5bfca4ec4bff27abe2d2d" "250007c5ae19bcbaa80e1bf8184720efb6262adafa9746868e6b9ecd9d5fbf84" "7dc296b80df1b29bfc4062d1a66ee91efb462d6a7a934955e94e786394d80b71" "3199be8536de4a8300eaf9ce6d864a35aa802088c0925e944e2b74a574c68fd0" "046e442b73846ae114d575a51be9edb081a1ef29c05ae5e237d5769ecfd70c2e" "e87f48ec4aebdca07bb865b90088eb28ae4b286ee8473aadb39213d361d0c45f" "4a288765be220b99defaaeb4c915ed783a9916e3e08f33278bf5ff56e49cbc73" "5a611788d47c1deec31494eb2bb864fde402b32b139fe461312589a9f28835db" "c414f69a02b719fb9867b41915cb49c853489930be280ce81385ff7b327b4bf6" "02fff7eedb18d38b8fd09a419c579570673840672da45b77fde401d8708dc6b5" "74a50f18c8c88eac44dc73d7a4c0bbe1f3e72ff5971aac38fcf354ddad0d4733" "aa72e5b41780bfff2ff55d0cc6fcd4b42153386088a4025fed606c1099c2d9b8" "d0fb0c463d5d61e93f920e0fd1aa4c023bf719874b4d08f7f473b46c4adc0682" "6fc03df7304728b1346091dd6737cb0379f348ddc9c307f8b410fba991b3e475" "31f1723fb10ec4b4d2d79b65bcad0a19e03270fe290a3fc4b95886f18e79ac2f" "feb8e98a8a99d78c837ce35e976ebcc97abbd8806507e8970d934bb7694aa6b3" default))
+ '(k8s-site-docs-version "v1.22")
+ '(org-babel-clojure-backend 'cider)
+ '(org-image-actual-width nil)
  '(package-selected-packages
-   '(ox-hugo git-link cmake-mode company-glsl glsl-mode zenburn-theme which-key sql-indent rainbow-delimiters projectile modus-vivendi-theme modus-operandi-theme magit leuven-theme kubel git-timemachine counsel company clj-refactor auto-compile ace-window))
+   '(yaml-imenu cfn-mode k8s-mode typescript-mode sql-indent org-tree-slide handlebars-mode ripgrep ace-window modus-themes which-key yaml-mode projectile paredit rainbow-delimiters git-link git-timemachine magit avy counsel swiper ivy company clj-refactor cider auto-compile))
  '(safe-local-variable-values
-   '((cider-clojure-cli-global-options . "-A:fig:dev")
+   '((cider-clojure-cli-aliases . ":dev")
+     (cider-clojure-cli-aliases . "build-new:piggieback:figwheel")
+     (cider-clojure-cli-aliases . ":utc:dev:test:headless")
+     (cider-clojure-cli-aliases . "build-new:cider-cljs:figwheel")
+     (cider-clojure-cli-aliases . "dev")
+     (cider-clojure-cli-aliases . ":dev:headless:hashp")
+     (cider-clojure-cli-aliases . ":dev:headless")
+     (cider-clojure-cli-aliases . :dev:headless)
+     (cider-clojure-cli-aliases . dev:headless)
+     (cider-clojure-cli-aliases . "web-dev:figwheel")
+     (cider-clojure-cli-aliases . "devcards:figwheel")
+     (cider-clojure-cli-aliases . "devcards:figwheel-main")
+     (cider-clojure-cli-aliases . "web:web-dev:figwheel-main")
+     (cider-clojure-cli-aliases . -A:figwheel)
+     (cider-clojure-cli-global-options . "-A:repl:cider-cljs")
+     (cider-clojure-cli-global-options . "-A:fig:dev")
      (cider-clojure-cli-global-options . "-A:figwheel-main-dev")
      (cider-clojure-cli-global-options . "-M:figwheel-main-dev")
      (cider-clojure-cli-global-options . "-A:dev")
@@ -20,7 +38,8 @@
  '(smtpmail-smtp-service 465)
  '(smtpmail-stream-type 'ssl)
  '(user-mail-address "me@mitrx.dev")
- '(visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow)))
+ '(visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
+ '(warning-suppress-types '((comp) (comp) (comp) (comp) (comp) (comp) (comp) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
